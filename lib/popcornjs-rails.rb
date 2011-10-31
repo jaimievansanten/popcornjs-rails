4ea44fd8be0175b70878df21aaf51a40c608090f
Jaimie van Santen <rhoxus@gmail.com>
2011-10-31 23:13:18 +0100

    Fix for asset pipeline in Rails 3.1

--- lib/popcornjs-rails.rb
+++ lib/popcornjs-rails.rb
@@ -2,6 +2,10 @@ require "popcornjs-rails/version"
 
 module Popcornjs
   module Rails
-    # Your code goes here...
+    if defined?(::Rails) and ::Rails.version >= "3.1"
+			class Rails::Engine < ::Rails::Engine
+				# this class enables the asset pipeline
+			end
+		end
   end
 end
